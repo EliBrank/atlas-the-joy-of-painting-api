8b3f461f45d42dad33d60820420c3686b01f9c43
EliBrank <ezb.business@pm.me>
2025-04-06 21:14:48 -0500

    create script to process dates from date source

--- src/transformers/transform-dates.ts
+++ src/transformers/transform-dates.ts
@@ -1,16 +1,40 @@
-import { parse } from 'date-fns';
 import extractDates from 'extrators/extract-dates.js';
 import normalizeTitle from 'utils/normalize-title.js';
 
 export interface EpisodeDateData {
   title: string;
-  airDate: Date;
+  date: Date;
 }
 
-export async function transformDates(): Promise<EpisodeDateData> {
+export async function transformDates(): Promise<EpisodeDateData[]> {
   const content = await extractDates();
   const lines = content.split('\n');
-  console.log(lines[0]);
+
+  return lines.map(line => {
+    // captures "title" (date) in two separate groups
+    const pattern = /"([^"]+)"\s*\((\w+\s\d{1,2},\s\d{4})\)/;
+    const match = line.match(pattern);
+
+    if (!match) {
+      throw new Error('Invalid data pattern: ${line}');
+    }
+    // match[0] contains the full matched string
+    // match[1], match[2]... contain capture groups
+    const rawTitle = match[1];
+    const rawDate = match[2];
+
+    const title = normalizeTitle(rawTitle);
+    if (!title) {
+      throw new Error('Invalid title: ${line}');
+    }
+
+    const date = new Date(rawDate);
+    if (isNaN(date.getTime())) {
+      throw new Error('Invalid date: ${line}');
+    }
+
+    return { title, date };
+  });
 }
 
-await transformDates();
+console.log(await transformDates());
